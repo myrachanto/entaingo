e9005fd56bd161c2b13077a73f36205d5210bd61
Antonio myracho <26426455+myrachanto@users.noreply.github.com>
2024-10-22 13:26:27 +0300

    swagger heading

--- main.go
+++ main.go
@@ -9,6 +9,17 @@ import (
 func init() {
 	log.SetPrefix("entaingo :==> ")
 }
+
+// @title Entaingo  API Documention
+// @version 1.0
+// @description This is a entaingo API Documention server.
+
+// @contact.name API Support
+// @contact.url https://www.chantosweb.com
+// @contact.email myrachanto1@gmail.io
+
+// @license.name Apache 2.0
+// @license.url http://www.apache.org/licenses/LICENSE-2.0.html
 func main() {
 	log.Println("server started..........")
 	routes.ApiServer()
